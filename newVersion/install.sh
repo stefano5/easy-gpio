3d0814bbaf354135cc0605246ebd6582702c3b79
Stefano <stefanomaugeri@hotmail.it>
2020-02-17 09:55:45 +0100

    update

--- newVersion/install.sh
+++ newVersion/install.sh
@@ -1,8 +1,7 @@
 sudo cp -r lib/* /usr/include/
 
-
 sudo systemctl stop daemon-easy-gpio.service > /dev/null
-suro rm /usr/bin/easy-gpio /usr/bin/daemon-easy-gpio > /dev/null
+sudo rm /usr/bin/easy-gpio /usr/bin/daemon-easy-gpio /usr/bin/daemon-easy-gpio.sh > /dev/null
 sudo rm /etc/systemd/system/daemon-easy-gpio.service > /dev/null
 
 gcc easy-gpio.c -o easy-gpio
@@ -10,6 +9,7 @@ gcc daemon-easy-gpio.c -lpthread -o daemon-easy-gpio
 sudo cp easy-gpio /usr/bin/
 sudo cp daemon-easy-gpio /usr/bin/
 
+sudo cp service/daemon-easy-gpio.sh /usr/bin/
 sudo cp service/daemon-easy-gpio.service /etc/systemd/system/daemon-easy-gpio.service
 
 sudo systemctl enable daemon-easy-gpio.service
